e39112bd054820a9d874a87a2d736e9225941222
Vikas <xanderbilla@gmail.com>
2025-01-20 22:36:43 +0530

    P1# Project Initialized

---
+++ src/main/java/com/myproject/projectapi/ProjectapiApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.projectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/projectapi/ProjectapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.projectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

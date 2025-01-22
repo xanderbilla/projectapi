62c09eb5e8500435cdcca8d581f67b207d28fbfe
Vikas <xanderbilla@gmail.com>
2025-01-23 04:04:52 +0530

    P12# Added /health for Target Group health check

--- src/main/java/com/myproject/projectapi/controller/HealthController.java
+++ src/main/java/com/myproject/projectapi/controller/HealthController.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myproject.projectapi.config.ApplicationStartupListener;
 
 @RestController
+@RequestMapping("/health")
 public class HealthController {
 
     private static final Logger logger = LoggerFactory.getLogger(ApplicationStartupListener.class);

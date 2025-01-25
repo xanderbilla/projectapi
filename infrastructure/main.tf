e0fb1b147490fdffbf4db82ed3f5bad1cf98193c
Vikas <xanderbilla@gmail.com>
2025-01-26 03:05:59 +0530

    Patch5$ AWS SSM Document

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -214,10 +214,9 @@ resource "aws_instance" "projectapi_ec2" {
                 sudo systemctl start docker
                 caller=$(aws sts get-caller-identity --query 'Account' --output text)
                 aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin $caller.dkr.ecr.ap-south-1.amazonaws.com
-                secret=$(aws secretsmanager get-secret-value --secret-id projectapi/image-repo --query 'SecretString' --output text | python -c "import json, sys; print(json.load(sys.stdin)['REPO'])")
-                sudo docker pull $secret:latest
-                sudo docker tag $secret my-spring-image
-                sudo docker rmi -f $secret:latest
+                sudo docker pull $caller.dkr.ecr.ap-south-1.amazonaws.com/myprojectapi:latest
+                sudo docker tag $caller.dkr.ecr.ap-south-1.amazonaws.com/myprojectapi my-spring-image
+                sudo docker rmi -f $caller.dkr.ecr.ap-south-1.amazonaws.com/myprojectapi:latest
                 secret=$(aws secretsmanager get-secret-value --secret-id mongo/connection-url --query 'SecretString' --output text | python -c "import json, sys; print(json.load(sys.stdin)['MONGO_URI'])")
                 sudo docker run -d -p 8080:8080 -e MONGO_URI=$secret --name spring-app my-spring-image
                 EOF
@@ -356,6 +355,37 @@ resource "aws_cloudwatch_event_rule" "ecr_image_push_rule" {
   EOT
 }
 
+resource "aws_ssm_document" "run_command" {
+    name          = "RunShellScript"
+    document_type = "Command"
+
+    content = jsonencode({
+        schemaVersion = "2.2"
+        description   = "Run shell script on EC2"
+        mainSteps = [
+            {
+                action = "aws:runShellScript"
+                name   = "runShellScript"
+                inputs = {
+                    runCommand = [
+                        "sudo docker stop spring-app || true",
+                        "sudo docker rm -f spring-app || true",
+                        "sudo docker rmi -f my-spring-image || true",
+                        "caller=$(aws sts get-caller-identity --query 'Account' --output text)",
+                        "aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin $caller.dkr.ecr.ap-south-1.amazonaws.com",
+                        "sudo docker pull $caller.dkr.ecr.ap-south-1.amazonaws.com/myprojectapi:latest",
+                        "sudo docker tag $caller.dkr.ecr.ap-south-1.amazonaws.com/myprojectapi my-spring-image",
+                        "sudo docker rmi -f $caller.dkr.ecr.ap-south-1.amazonaws.com/myprojectapi:latest",
+                        "secret=$(aws secretsmanager get-secret-value --secret-id mongo/connection-url --query 'SecretString' --output text | jq -r .MONGO_URI)",
+                        "sudo docker run -d -p 8080:8080 -e MONGO_URI=$secret --name spring-app my-spring-image"
+                    ]
+                }
+            }
+        ]
+    })
+}
+
+
 resource "aws_iam_role" "eventbridge_ssm_role" {
   name = "eventbridge-ssm-role"
   assume_role_policy = <<EOF
@@ -383,7 +413,8 @@ resource "aws_iam_role_policy_attachment" "eventbridge_ssm_attach" {
 resource "aws_cloudwatch_event_target" "ecr_event_target" {
     rule      = aws_cloudwatch_event_rule.ecr_image_push_rule.name
     target_id = "ssm-command-target"
-    arn       = "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:document/projectapi-update-container"
+    # arn     = "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:document/projectapi-update-container"
+    arn       = aws_ssm_document.run_command.arn
 
     run_command_targets {
         key    = "InstanceIds"
